80ad91dc3d3a3edc24c603768be1cd0e5f31a8a1
Muslim245 <doolsekaa2@gmail.com>
2025-06-15 00:58:34 +0300

    Modulation

--- src/Component/Products/Products.jsx
+++ src/Component/Products/Products.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { CartContext } from "../../Context/CartContext"
 import { ContextList } from "../../Context/ContextList"
+import { useFormik } from "formik"
 export function Products() {
    let {getCardData , idProduct , array } = useContext(CartContext)
    let {addList , idList , arr  } = useContext(ContextList)
@@ -10,10 +11,22 @@ export function Products() {
    let [load, setload] = useState(true)
    let [loadAdd, setloadAdd] = useState(false)
    let [loadList, setloadList] = useState(false)
+   let [allProducts, setAllProducts] = useState([]);
+   let formik = useFormik({
+    initialValues : {
+        search : ""
+    },
+   })
+  useEffect(() => {
+  let saveValue = formik.values.search.trim().toLowerCase();
+   setapi(allProducts.filter((item)=>item.title.trim().toLowerCase().includes(saveValue)))
+}, [formik.values.search, allProducts]);
+
    async function getData () {
    try{
     let {data} = await axios.get(`https://ecommerce.routemisr.com/api/v1/products`)
     setapi(data.data)
+    setAllProducts(data.data);
     setload(false)
    }
    catch(error)
@@ -32,7 +45,20 @@ export function Products() {
         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
     </svg>
 </div>
-  :<div className=" row flex flex-wrap w-3/4 gap-5 mx-auto pt-28 pb-10">
+  : <>
+  <div className=" w-3/4 mx-auto pt-28">
+ <form onSubmit={formik.handleSubmit} action="" className="flex flex-col gap-2">
+     <label htmlFor="search">Search</label>
+     <input className=" w-full border border-black border-solid outline-none rounded-md py-2 px-3"
+      type="text" name="search" id="search"
+      placeholder="Search In Prouducts"
+      value={formik.values.search}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      />
+ </form>
+  </div>
+  <div className=" row flex flex-wrap w-3/4 gap-5 mx-auto pt-10 pb-10">
       {api.map((item,index)=><div className="flex flex-col justify-between w-full md:w-[47%] lg:w-[23%] p-5 group rounded-lg duration-500 hover:shadow-[1px_1px_10px_1px] hover:shadow-[#4fa74f]"  key={index} >
       <Link to={`/Details/${item.id}/${item.category.name}`}>
       <div>
@@ -57,6 +83,7 @@ export function Products() {
      </div>
      </div>
    )}</div>
+ </>
    }
     </>
 }
